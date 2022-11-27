5bbf264ecaea6178e5a73369c85c13be9f6d8efc
Dixit <diks@gmail.com>
2022-11-27 19:07:34 -0400

    Forget Password Fragment

--- client/app/src/main/java/com/dalhousie/foodnculture/ForgetPasswordFragment.java
+++ client/app/src/main/java/com/dalhousie/foodnculture/ForgetPasswordFragment.java
@@ -9,7 +9,9 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-public class SecondFragment extends Fragment {
+import com.google.android.material.snackbar.Snackbar;
+
+public class ForgetPasswordFragment extends Fragment {
 
     @Override
     public View onCreateView(
@@ -17,7 +19,7 @@ public class SecondFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        return inflater.inflate(R.layout.fragment_forget_password, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
@@ -26,8 +28,8 @@ public class SecondFragment extends Fragment {
         view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+                Snackbar.make(view, "Reset link has been sent successfully to registered email.", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
     }

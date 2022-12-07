8ddc5acc64f5cdecee5a90d8e9a89dcf7a3c067c
Sumit Kumar <sm585558@dal.ca>
2022-12-07 18:35:20 -0400

    update persistance layer to use stored procedure

--- server/src/main/java/com/dalhousie/server/persistence/EventRepository.java
+++ server/src/main/java/com/dalhousie/server/persistence/EventRepository.java
@@ -18,23 +18,20 @@ public class EventRepository implements IEventRepository {
 
     @Override
     public List<Event> findAll() {
-        return jdbcTemplate.query("SELECT * FROM events", BeanPropertyRowMapper.newInstance(Event.class));
+        return jdbcTemplate.query("CALL getAllEvents()", BeanPropertyRowMapper.newInstance(Event.class));
     }
 
     @Override
     public int save(Event event) {
-        System.out.println(event.toString());
-        return jdbcTemplate.update(
-                "INSERT INTO events(id, titlle, description, event_type, status, start_datetime, end_datetime, venue, max_capacity, updated_at, created_at) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                event.getId(), event.getTitle(), event.getDescription(), event.getEventType().toString(), event.getStatus(),
-                event.getStartDateTime(), event.getEndDateTime(), event.getVenue(), event.getMaxCapacity(),
-                event.getUpdatedAt(), event.getCreatedAt());
+        return jdbcTemplate.update("CALL createEvent(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", event.getId(), event.getTitle(), event.getDescription(), event.getEventType().toString(), event.getStatus(),
+        event.getStartDateTime(), event.getEndDateTime(), event.getVenue(), event.getMaxCapacity(),
+        event.getUpdatedAt(), event.getCreatedAt());
     }
 
     @Override
     public int update(Event event) {
         return jdbcTemplate.update(
-                "UPDATE events SET titlle=?, description=?, event_type=?, status=?, start_datetime=?, end_datetime=?, venue=?, max_capacity=?, updated_at=?, created_at=? WHERE id=?",
+                "CALL updateEvent(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
                 event.getTitle(), event.getDescription(), event.getEventType().toString(), event.getStatus(),
                 event.getStartDateTime(), event.getEndDateTime(), event.getVenue(), event.getMaxCapacity(),
                 event.getUpdatedAt(), event.getCreatedAt(), event.getId());
@@ -42,12 +39,12 @@ public class EventRepository implements IEventRepository {
 
     @Override
     public int delete(Event event) {
-        return jdbcTemplate.update("DELETE FROM events WHERE id=?", event.getId());
+        return jdbcTemplate.update("CALL deleteEventById(?)", event.getId());
     }
 
     @Override
     public int deleteById(Integer id) {
-        return jdbcTemplate.update("DELETE FROM events WHERE id=?", id);
+        return jdbcTemplate.update("CALL deleteEventById(?)", id);
     }
 
     @Override
@@ -58,7 +55,7 @@ public class EventRepository implements IEventRepository {
     @Override
     public Optional<Event> getById(Integer id) {
         try {
-            Event event = jdbcTemplate.queryForObject("SELECT * from events WHERE id=?", BeanPropertyRowMapper.newInstance(Event.class), id);
+            Event event = jdbcTemplate.queryForObject("CALL getEventById(?)", BeanPropertyRowMapper.newInstance(Event.class), id);
             return Optional.of(event);
         }catch(Exception e) {
             return Optional.empty();

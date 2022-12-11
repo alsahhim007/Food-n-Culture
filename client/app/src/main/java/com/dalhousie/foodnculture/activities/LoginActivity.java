fe4046061eb924d4eeed1d57222fb0a75bce5eea
Sumit Kumar <sm585558@dal.ca>
2022-12-11 15:50:25 -0400

    fix missing import.

--- client/app/src/main/java/com/dalhousie/foodnculture/activities/LoginActivity.java
+++ client/app/src/main/java/com/dalhousie/foodnculture/activities/LoginActivity.java
@@ -2,6 +2,7 @@ package com.dalhousie.foodnculture.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
